548def6267242ca76f4cd4b9fd2f8f8ce618817c
Oscar Palm <palmoscar4@gmail.com>
2024-02-15 16:44:25 +0100

    Small ts router

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import { config } from "dotenv";
+import express from "express";
+import assignmentRouter from "./routers/assignmentRouter";
+
+const app = express();
+config();
+
+app.use("/assignments", assignmentRouter)
+
+app.get("/:name", (req, res) => {
+	const name = req.params.name;
+	if (!name)
+		res.status(200).send("Hej =)");
+	else res.status(200).send(`Hej ${name}`);
+});
+
+app.listen(process.env.PORT || 3000, () => { console.log(`Server started on ${process.env.PORT || 3000}`) });
---
+++ backend/src/routers/assignmentRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+
+const assignmentRouter = Router();
+assignmentRouter.get("/next", (req, res) => { console.log("OoOps, not implemented") });
+
+export default assignmentRouter;
