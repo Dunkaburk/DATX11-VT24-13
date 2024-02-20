17f41d183d829a41aa7bcb17d9ff2fd600650b4b
Oscar Palm <palmoscar4@gmail.com>
2024-02-20 09:28:51 +0100

    now send 501 when not implemented

--- backend/src/routers/assignmentRouter.ts
+++ backend/src/routers/assignmentRouter.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 
 const assignmentRouter = Router();
-assignmentRouter.get("/next", (req, res) => { console.log("OoOps, not implemented") });
-assignmentRouter.get("/previous", (req, res) => { console.log("OoOps, not implemented") });
-assignmentRouter.post("/submit", (req, res) => { console.log("OoOps, not implemented") });
+assignmentRouter.get("/next", (req, res) => { res.status(501).send("OoOps, not implemented") });
+assignmentRouter.get("/previous", (req, res) => { res.status(501).send("OoOps, not implemented") });
+assignmentRouter.post("/submit", (req, res) => { res.status(501).send("OoOps, not implemented") });
 
 export default assignmentRouter;
