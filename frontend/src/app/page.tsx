5807c03e68331a47b9c500eac2cace21558a2860
Oscar Palm <palmoscar4@gmail.com>
2024-03-07 15:14:09 +0100

    Simple showing of how data fetching works

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+import callAPI from "@/hooks/apiHooks";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [ass, useAss] = useState('');
+
+  useEffect(() => {
+    callAPI({ route: "/api/student/getAll", method: "GET", data: {} }).then((res) => useAss(res));
+  }, []);
+
   return (
     <main className={styles.main}>
-      Hej
+      {ass}
     </main>
   );
 }
