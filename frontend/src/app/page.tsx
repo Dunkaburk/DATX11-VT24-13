71cd6ab7ed82b07b8f632c68388c9673b65b2115
Oscar Palm <palmoscar4@gmail.com>
2024-03-08 10:58:10 +0100

    Add super simple card for categories/questions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,17 +2,19 @@
 import callAPI from "@/hooks/apiHooks";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
+import Card from "@/components/Card";
 
 export default function Home() {
-  const [ass, useAss] = useState('');
+  const [cat, useCat] = useState({ "categories": [] }); // https://www.youtube.com/watch?v=v0nvwwdeV6I
 
   useEffect(() => {
-    callAPI({ route: "/api/student/getAll", method: "GET", data: {} }).then((res) => useAss(res));
+    callAPI({ route: "/api/category/getAll", method: "GET", data: {} }).then((res) => useCat(res));
   }, []);
 
   return (
     <main className={styles.main}>
-      {ass}
+      {(cat.categories) ?
+        cat.categories.map((category) => (<Card title={category} onClick={() => { console.log("Hej =)"); }} id={""} key={category} />)) : <div>Loading...</div>}
     </main>
   );
 }
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,21 @@
+export interface CardProps {
+	title: string;
+	description?: string;
+	onClick?: (x: string) => void;
+	id?: string;
+}
+
+const Card = ({ title, description = "", onClick = (x) => { console.log("no function attached to card"); }, id = "" }: CardProps) => {
+	return (
+		<div
+			className="card"
+			onClick={() => (onClick(id))}
+			onTouchEnd={() => { onClick(id) }}
+		>
+			<h2>{title}</h2>
+			<p>{description}</p>
+		</div >
+	);
+}
+
+export default Card;
