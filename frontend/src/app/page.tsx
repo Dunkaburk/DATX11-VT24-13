a9753b343e47f87fe0b8d91dc941216bc00dfd13
Oscar Palm <palmoscar4@gmail.com>
2024-03-12 15:10:41 +0100

    Add simple

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,15 +6,23 @@ import Card from "@/components/Card";
 
 export default function Home() {
   const [cat, useCat] = useState({ "categories": [] }); // https://www.youtube.com/watch?v=v0nvwwdeV6I
-
+  const [assignment, useAssignment] = useState({ "assignments": [] });
+  const changeCategory = (category: string) => {
+    callAPI({ route: `/api/category/get/${category}`, method: "GET", data: {} }).then((res) => useAssignment(res));
+    console.log(assignment);
+  }
   useEffect(() => {
     callAPI({ route: "/api/category/getAll", method: "GET", data: {} }).then((res) => useCat(res));
   }, []);
+  /* useEffect(() => {
+
+  }, []); */
+
 
   return (
     <main className={styles.main}>
       {(cat.categories) ?
-        cat.categories.map((category) => (<Card title={category} onClick={() => { console.log("Hej =)"); }} id={""} key={category} />)) : <div>Loading...</div>}
+        cat.categories.map((category) => (<Card title={category} onClick={(categor) => { changeCategory(categor) }} id={category} key={category} />)) : <div>Loading...</div>}
     </main>
   );
 }
