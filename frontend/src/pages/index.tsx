5f2fb3b0f69d5dc0ad65e3c115dac5772da1a714
Oscar Palm <palmoscar4@gmail.com>
2024-03-13 11:41:22 +0100

    flips the list when selecting module

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const [assignment, useAssignment] = useState({ "assignments": [] });
   const changeCategory = (category: string) => {
     callAPI({ route: `/api/category/get/${category}`, method: "GET", data: {} }).then((res) => { useAssignment(res); console.log(res); });
-
   }
   useEffect(() => {
     callAPI({ route: "/api/category/getAll", method: "GET", data: {} }).then((res) => { useCat(res); });
@@ -17,8 +16,10 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      <div className={assignment.assignments.length > 0 ? styles.assignmentWheel : styles.assignmentList}>
       {(cat.categories) ?
         cat.categories.map((category) => (<Card title={category} onClick={(categor) => { changeCategory(categor) }} id={category} key={category} />)) : <div>Loading...</div>}
+      </div>
       {(assignment.assignments) ?
         assignment.assignments.map((assignment) => (<Card title={assignment} id={assignment} key={assignment} />)) : <></>}
     </main>
